ae2f4f9d08076506f84f078fd397f0f8a34d185e
snhynz <snhynz@gmail.com>
2022-07-25 19:09:24 +0300

    First Spring

---
+++ src/main/java/com/example/firstSpringProjectMe/FirstSpringProjectMeApplication.java
@@ -0,0 +1,14 @@
+package com.example.firstSpringProjectMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringProjectMeApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FirstSpringProjectMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/firstSpringProjectMe/FirstSpringProjectMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstSpringProjectMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringProjectMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
